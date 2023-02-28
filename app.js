95872f6883d5eccbaeae7fea99b1b2e7ea8bfbe4
Jieun Kim <jieunkim1379@gmail.com>
2023-02-28 11:29:52 -0500

    finished

--- app.js
+++ app.js
@@ -1,10 +1,82 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require("./postBank");
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan("dev"));
+app.use(express.static("public"));
 
-const PORT = 1337;
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel='stylesheet' href='/style.css' />
+  </head>
+  <body>
+  <div class="news-list">
+  <header><img src="/logo.png"/>Wizard News</header>
+  ${posts
+    .map(
+      (post) => `
+    <div class='news-item'>
+      <p>
+        <span class="news-position">${post.id}. ▲</span>
+        <a href="/posts/${post.id}">${post.title}</a>
+        <small>(by ${post.name})</small>
+      </p>
+      <small class="news-info">
+        ${post.upvotes} upvotes | ${post.date}
+      </small>
+    </div>`
+    )
+    .join("")}
+</div>
+  </body>
+  </html>`;
 
+  res.send(html);
+});
+
+app.get("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel='stylesheet' href='/style.css' />
+  </head>
+  <body>
+  <div class="news-list">
+  <header><img src="/logo.png"/>Wizard News</header>
+  <div class='news-item'>
+    <p>${post.title}
+      <small>(by ${post.name})</small>
+    </p>
+    <p>${post.content}</p>
+  </div>
+  </div>
+  </body>
+  </html>`;
+
+  if (post.id) {
+    res.send(html);
+  } else {
+    next(error);
+  }
+});
+
+app.use((error, req, res, next) => {
+  res.status(404).send({
+    error: "404 - not found",
+    message: "no route found for the request url",
+  });
+});
+
+const { PORT = 1337 } = process.env;
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
